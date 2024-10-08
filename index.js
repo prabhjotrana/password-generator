828a412a3bf51286dac3c12bf3e0d625f3aee5f2
prabhjotrana <37528179+prabhjotrana@users.noreply.github.com>
2024-10-09 03:18:13 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let password1=document.getElementById("password1");
+let password2=document.getElementById("password2");
+function generatePassword() {
+    for (let i = 0; i < 15; i++) {
+        {
+            let count=Math.floor(Math.random()*characters.length)+1;
+            password1.textContent+=characters[count];
+        }
+        
+    }
+    for (let i = 0; i < 15; i++) {
+        {
+            let count=Math.floor(Math.random()*characters.length)+1;
+            password2.textContent+=characters[count];
+        }
+        
+    }
+}
